fb3893206e9d6dd7b8ae5866bc25d95e01fce60a
Ayush-2965 <ayush4449kumar@gmail.com>
2025-03-22 01:27:20 +0530

    updated root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,45 @@
-import { Stack } from "expo-router";
-import './globals.css'
+import { router, Slot, Stack, useRouter } from "expo-router";
+import { useEffect, useState } from "react";
+import { getCurrentUser, getSession } from "../utils/appwrite"; // Function to check session
+import { ActivityIndicator, View } from "react-native";
+
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" options={{headerShown: false}} />
-    </Stack>);
+    const [isLoading, setLoading] = useState(true);
+    const [isAuthenticated, setAuthenticated] = useState(false);
+const router=useRouter();
+    useEffect(() => {
+        async function checkAuth() {
+            const session = await getSession();
+            const acc= await getCurrentUser();
+            // console.log(acc);
+            setAuthenticated(!!acc); // acc or session
+            setLoading(false);
+            // console.log(session);
+        }
+        checkAuth();
+    }, []);
+    // console.log(isAuthenticated);
+   
+    useEffect(() => {
+        if (!isLoading) {
+            if (isAuthenticated) {
+                router.replace("/(tabs)"); // Redirect logged-in users to tabs
+            } else {
+                router.replace("/");
+            }
+        }
+    }, [isLoading, isAuthenticated]);
+    
+    if (isLoading) {
+        return (
+            <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                <ActivityIndicator size="large" color="#0000ff" />
+            </View>
+        );
+    }
+
+    return (
+       
+        <Slot/>
+    );
 }
