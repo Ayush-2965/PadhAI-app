6633c660917f60baa9c5692fc3d123953b05d40d
Ayush-2965 <ayush4449kumar@gmail.com>
2025-03-24 01:01:50 +0530

    added svg support

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,18 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require('nativewind/metro');
+const { withNativeWind } = require("nativewind/metro");
 
-const config = getDefaultConfig(__dirname)
+const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './app/globals.css' })
+// ✅ Configure Metro to process SVGs as React components
+config.transformer = {
+  ...config.transformer,
+  babelTransformerPath: require.resolve("react-native-svg-transformer"),
+};
+
+config.resolver = {
+  ...config.resolver,
+  assetExts: config.resolver.assetExts.filter(ext => ext !== "svg"), // Remove SVG from asset extensions
+  sourceExts: [...config.resolver.sourceExts, "svg"], // Add SVG to source extensions
+};
+
+module.exports = withNativeWind(config, { input: "./app/globals.css" });
