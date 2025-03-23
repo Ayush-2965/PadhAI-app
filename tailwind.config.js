c83e57c82e7a7b8a27ed3f7628b0ad3b83692576
Ayush-2965 <ayush4449kumar@gmail.com>
2025-03-24 01:01:21 +0530

    fonts-added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}", // Your app pages
+    "./components/**/*.{js,jsx,ts,tsx}", // Components directory
+  ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Poppins-400", "sans-serif"], 
+        poppins: ["Poppins-300", "Poppins-400", "Poppins-500","Poppins-600", "Poppins-700", "Poppins-800"],
+        sf: ["SF-400", "SF-700"],
+      },
+    },
   },
   plugins: [],
-}
+};
